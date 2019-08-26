85d196f6bc7a70e73d7952031562a63994c843f6
Manoj M S <manojsrinath97@gmail.com>
2019-08-26 11:43:56 +0530

    AppComponent-increment-decrement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app1';
+  //Instance Data Members
+  count = 0;
+
+
+  //Instance Methods
+  increment(/*this=receive reference of object*/){
+    ++this.count;
+  }
+
+  decrement(/*this=receive reference of object*/){
+    --this.count;
+  }
 }
